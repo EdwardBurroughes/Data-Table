dd18f0c00af7fcf590400aba9f61338b2cf5b430
Edward Burroughes <edward@land.tech>
2023-06-15 15:54:55 +0100

    rm only

--- src/components/Search.test.tsx
+++ src/components/Search.test.tsx
@@ -3,15 +3,14 @@ import userEvent from "@testing-library/user-event";
 import { Search } from "./Search";
 
 describe("search component", () => {
-  //TODO rm onlys
-  it.only("has a search icon", async () => {
+  it("has a search icon", async () => {
     render(<Search onChange={() => null} />);
     // find by if it's there
     const searchIconElement = await screen.findByTestId("search-icon");
     expect(searchIconElement).toBeInTheDocument();
   });
 
-  it.only("call onChange with search input", async () => {
+  it("call onChange with search input", async () => {
     const mockOnChange = jest.fn();
     render(<Search onChange={mockOnChange} />);
     userEvent.type(screen.getByTestId("search-input"), "A");
